a72a5e09353c80f926d918f4fff493e8455a65db
dasoya <ekth020216@gmail.com>
2021-12-10 18:39:16 +0900

    1210 Today's sign radioBtn

--- src/makingSignView.js
+++ src/makingSignView.js
@@ -11,6 +11,50 @@ import J_List from './components/J_List';
 import { NavigationContainer } from '@react-navigation/native';
 import { IconButton} from 'react-native-paper';
 import { goBack } from './J_index';
+import RadioButton from './components/J_radioButton';
+
+const PROP = [
+	{
+		key: '1',
+		text: 'pic1',
+    image: images.pic1,
+    selimage: images.sPic1
+	},
+  {
+		key: '2',
+		text: 'pic2',
+    image: images.pic2,
+    selimage: images.sPic2
+	},
+  {
+		key: '3',
+		text: 'pic3',
+    image: images.pic3,
+    selimage: images.sPic3
+	},
+  {
+		key: '4',
+		text: 'pic4',
+    image: images.pic4,
+    selimage: images.sPic4
+	},
+  {
+		key: '5',
+		text: 'pic5',
+    image: images.pic5,
+    selimage: images.sPic5
+	},
+];
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
 
 const makeSign= ({ navigation, route }) => {
 
@@ -30,16 +74,17 @@ const makeSign= ({ navigation, route }) => {
             <Text style={textStyles.title}>Today's Sign</Text>
             <Input />
             <TrafficSign doneListNum={5} totalListNum={115} />
-            <View style={viewStyles.test}><J_List /></View>
+            <View style={styles.container}>
+
+            <RadioButton PROP={PROP} />
+
+            </View>
+            {/* <View style={viewStyles.test}><J_List /></View> */}
           </View>
           </TouchableWithoutFeedback>
           
           <View style={viewStyles.footer}>
             <View>
-            {/* <Button
-      title="Go to Jane's profile"
-      onPress={() => navigation.navigate('showSign')}
-    /> */}
               <IconButton icon={images.done} onPress={() => navigation.navigate('showSign') }/>
             </View>
           </View>
