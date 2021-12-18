93a7ee7b4d85ea654ca3bbe84dfaa9c7b304feaa
dasoya <ekth020216@gmail.com>
2021-12-19 00:19:45 +0900

    restore, J-list

--- src/components/J_List.js
+++ src/components/J_List.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react';
-import {Image,FlatList,View} from 'react-native';
+import React, {Component,TouchableOpacity} from 'react';
+import {Image,FlatList,View,Text} from 'react-native';
+import { List } from 'react-native-paper';
 import picListData from '../J_picListData';
 import { PicButton } from './IconButton';
 
@@ -26,6 +27,43 @@ class J_List extends Component {
                         //console.log(`Item=${JSON.stringify(item)}, index= ${index}`)
                         return(
                             <PicButton type = {item.img}/>                            
+                        );
+                    }}
+               />
+           </View>
+        );
+    }
+}
+
+export class Todo_List extends Component {
+
+    
+    render() {
+        var ListData = this.props.data;
+        const listArray = Object.values(ListData);
+        console.log("list : ",Object.values(ListData));
+        return (
+           <View style={{flex: 1,  marginTop: 22 , justifyContent: 'center'}}>
+               <FlatList
+                    data ={listArray}
+                    horizontal = {false}
+                    renderItem = {({item,index})=>{
+                        
+                        console.log(`Item=${JSON.stringify(item)}, index= ${index}`)
+                        return(
+                            //<Text >{item.ToDo}</Text> 
+                          // <Text>{item}a</Text>   
+
+                          //<TouchableOpacity   onPress={() => navigation.navigate('toDo') }> 
+                          <View style={{ padding:15, borderBottomWidth: 1, borderColor: "black", flexDirection: "row" }}>
+                          
+                           
+                              <Text>{item.ToDo}</Text>
+                          
+                            
+                          </View>
+                       // </TouchableOpacity>
+
                         );
                     }}
                />
