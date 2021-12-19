a29f9612c1ff9467327fea1460d583afd389eac2
journey_park <80277274+jihyeun-park@users.noreply.github.com>
2021-12-19 20:14:25 +0900

    1219 J_trafficSign

--- src/components/J_trafficSign.js
+++ src/components/J_trafficSign.js
@@ -29,28 +29,59 @@ export const ShowTrafficSign = ({trafficSign}) => {
   return (
         <Image source = {image} style = {trafficStyle.icon} />
   );
+
+
+//수정 data -> 다만.. doc.data() 불러오는게 맞는지
+// -> Flag : completed 여부 
+
+function Flag() {
+const todoRef = DB.collection('Todo');
+        
+todoRef.get().then((snapshot)=>{
+    snapshot.forEach((doc) =>{
+     
+      var key;
+      var value;
+      key = doc.id
+      value = doc.data();
+
+      //if( 카테고리에는 전부 띄우고 (왜냐면 미완료된일만 표시 때문에 flag=true도 띄움)
+      //doc.data().Flag == false){
+        todoData[key] =value;
+      //}
+     });
+     settodoData(todoData);
+     console.log("TODODATA1" ,todoData);
+});
+
+if(length(doc.data())*(0.7) <= length(doc.data().Flag == true)) {
+  return 1;
+} 
+else if ((length(doc.data())*(0.5) <= length(doc.data().Flag == true)) &&
+ (length(doc.data())*(0.5) > length(doc.data().Flag == true))) {
+  return 2;
+}
 };
-
-
 
 function PrintTrafficSign({setTraffic}){
     
-
-   let donePercent = 0
-  // donePercent = doneListNum*100/totalListNum 
-    // console.log('donePercent : %d %d %d',donePercent,doneListNum,totalListNum)
-    if (donePercent>=70){
-     setTraffic("2");
-      return  images.greenSign
-    } else if (donePercent>=50){
-     setTraffic("1");
-      return images.yellowSign
-    } else {
-     setTraffic("0");
-      return images.redSign
-    }
+  var donePercent =Flag();
+  
+ // donePercent = doneListNum*100/totalListNum 
+   // console.log('donePercent : %d %d %d',donePercent,doneListNum,totalListNum)
+   if (donePercent>=70){
+    setTraffic("2");
+     return  images.greenSign
+   } else if (donePercent>=50){
+    setTraffic("1");
+     return images.yellowSign
+   } else {
+    setTraffic("0");
+     return images.redSign
+   }
 
 }
+};
 
 const trafficStyle = StyleSheet.create({
     icon: {
